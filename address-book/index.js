cdcc5443a1992d74f11a73e906204696cfa8d006
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2018-12-20 17:01:50 +0700

    Create and modify index for address book page

---
+++ address-book/index.js
@@ -0,0 +1,9 @@
+console.log("Address Log");
+
+const CONTACTS = [
+    {
+        fullName:  "Desi Mandasari ",
+        nickName: "Manda",
+        email =["desimandasari1993@gmail.com", "desymanda93@gmail.com"]
+    }
+]
